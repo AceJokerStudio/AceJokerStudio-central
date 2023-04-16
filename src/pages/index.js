6bebab270c504b1b7b39507ff4842050f553b469
acej0k3r <ace.rav3n@gmail.com>
2023-04-15 23:12:25 -0400

    first message

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>AceJoker Studio | Home</title>
+        <meta name="description" content="The homepage of ace joker studio a subsidiary of acejokercapital.com. This site contains all applications created by Ace Joker Studio a subsidiary of AceJoker Capital Inc." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/AJS.png" />
+      </Head>
+
+      <main>
+
+      </main>
+
+    </>
+  )
+}
