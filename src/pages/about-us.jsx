3091a26784eda06c7d1c554fe577947a651e4a89
acej0k3r <ace.rav3n@gmail.com>
2023-04-29 06:46:46 -0400

    final build

---
+++ src/pages/about-us.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import s from '@/styles/sass/aboutus.module.scss'
+
+
+const aboutus = () => {
+  //standard
+
+  //state
+
+  //function
+
+
+  return (
+    <>
+      <pre>Welcome to AceJoker Studio, a subsidiary of AceJoker Capital Inc. We are a team of creative and innovative professionals dedicated to designing and developing smaller-scale applications and projects, as well as exploring other technologies and art.<br /><br />
+
+        Our mission at AceJoker Studio is to provide our clients with high-quality and user-friendly products that cater to their needs and exceed their expectations. We specialize in various areas, including mobile app development, web development, game design, graphic design, and more. Our expertise and experience allow us to offer a comprehensive range of services that are tailored to meet the demands of our clients.<br /><br />
+
+        As a subsidiary of AceJoker Capital Inc., we are proud to be part of a larger organization that has a strong focus on financial education and investments. While AceJoker Capital Inc. focuses on providing valuable financial knowledge and guidance to individuals and businesses, AceJoker Studio was designed to explore other areas and expand our creativity beyond financial matters. Our team comprises experts in different fields who bring their unique skills and perspectives to each project we undertake.<br /><br />
+
+        At AceJoker Studio, we are committed to delivering exceptional results that not only meet but also exceed our clients' expectations. We strive to maintain an open and transparent communication channel with our clients throughout the project's lifecycle, ensuring that they are always aware of the progress and are satisfied with the end result.<br /><br />
+
+        We are excited about the possibilities that lie ahead, and we look forward to working with you to bring your ideas to life. Thank you for choosing AceJoker Studio.</pre>
+    </>
+  )
+}
+
+
+export default aboutus;
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import s from '@/styles/sass/services.module.scss'
 
 
 const services = () => {
@@ -11,7 +12,21 @@ const services = () => {
 
    return (
       <>
-         services
+         <div className={` ${s['container']} `} >
+            <section className={` ${s['section1']} `} >
+               <h1>Services</h1>
+               <p>Here at AceJoker Studio a subsidiary of AceJoker Capital we value our contribution to society and the tech community. Majority of our main services are in our parent company <a href='https://acejokercapital.com' target='_blank'>AceJoker Capital Inc.</a> </p>
+
+               <div><button>Learn More</button></div>
+
+            </section>
+
+            <section className={` ${s['section2']} `} >
+
+            </section>
+
+         </div>
+
       </>
    )
 }
